45d1d838810c3d6b19420c6e27185005a94c1ef8
Raul Correa <raulcorreainformatica@gmail.com>
2026-02-28 03:57:07 +0000

    Atributo, constructor class Libro

--- proyecto/src/Modelo/Libro.java
+++ proyecto/src/Modelo/Libro.java
@@ -1,4 +1,25 @@
 package Modelo;
 
 public class Libro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int añoPublicacion;
+    private String editorial;
+    private EnumGenero genero;
+    private int totalCopias;
+    private int copiasDisponibles;
+    private EnumEstado estado;
+
+    public Libro(String isbn, String titulo, String autor, int añoPublicacion, String editorial, EnumGenero genero, int totalCopias) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.añoPublicacion = añoPublicacion;
+        this.editorial = editorial;
+        this.genero = genero;
+        this.totalCopias = totalCopias;
+        this.copiasDisponibles = totalCopias;
+        this.estado = EnumEstado.DISPONIBLE;
+    }
 }
