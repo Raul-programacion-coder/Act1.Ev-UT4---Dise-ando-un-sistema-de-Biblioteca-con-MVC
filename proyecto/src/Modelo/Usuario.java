e6e716d6cc879affd3b778f79136754b5b134fb5
Raul Correa <raulcorreainformatica@gmail.com>
2026-02-28 04:14:26 +0000

    Atributos, Constructor Usuario

--- proyecto/src/Modelo/Usuario.java
+++ proyecto/src/Modelo/Usuario.java
@@ -1,4 +1,20 @@
 package Modelo;
+import excepciones.MaxLibrosPrestadosException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Usuario {
+
+    private String id;
+    private String nombre;
+    private List<Prestamo> prestamosActivos = new ArrayList<>();
+    private List<Prestamo> historialPrestamos = new ArrayList<>();
+
+
+    public Usuario(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+
 }
