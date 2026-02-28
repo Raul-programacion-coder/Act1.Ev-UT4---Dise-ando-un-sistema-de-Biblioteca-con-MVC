6c7dc1b6b89952a193ea00893560f627699ebf50
Raul Correa <raulcorreainformatica@gmail.com>
2026-02-28 04:16:41 +0000

    Metodos de Usuario

--- proyecto/src/Modelo/Usuario.java
+++ proyecto/src/Modelo/Usuario.java
@@ -16,5 +16,26 @@ public class Usuario {
         this.nombre = nombre;
     }
 
+    public void agregarPrestamo(Prestamo p) throws MaxLibrosPrestadosException {
+        if (prestamosActivos.size() >= 3)
+            throw new MaxLibrosPrestadosException("Máximo 3 libros prestados");
+        prestamosActivos.add(p);
+    }
+
+    public void devolverPrestamo(Prestamo p) {
+        prestamosActivos.remove(p);
+        historialPrestamos.add(p);
+    }
+
+    public boolean puedePedirLibro(String isbn) {
+        for (Prestamo p : historialPrestamos) {
+            if (p.getLibro().getIsbn().equals(isbn)) {
+                if (!p.puedeReprestarse())
+                    return false;
+            }
+        }
+        return true;
+    }
+    
 
 }
